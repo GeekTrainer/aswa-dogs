c03f37960036a4ad79db61d66a00de2477cf518a
Christopher Harrison <chrhar@microsoft.com>
2020-08-13 10:07:04 -0700

    Added the modal

--- index.js
+++ index.js
@@ -10,16 +10,29 @@ main();
 
 function main() {
     displayDogs();
+    document.getElementById('dog-save')
+        .addEventListener('click', saveDog);
 }
 
 function displayDogs() {
-    const dogsListUI = document.getElementById('dog-list');
     for (let dog of dogs) {
-        const dogItem = document.createElement('a');
-        dogItem.href = '#';
-        dogItem.innerText = dog.name;
-        dogItem.className = 'list-group-item';
-
-        dogsListUI.appendChild(dogItem);
+        addDogToDisplay(dog);
     }
+}
+
+function addDogToDisplay(dog) {
+    const dogItem = document.createElement('a');
+    dogItem.href = '#';
+    dogItem.innerText = dog.name;
+    dogItem.className = 'list-group-item';
+
+    const dogsListUI = document.getElementById('dog-list');
+    dogsListUI.appendChild(dogItem);
+}
+
+function saveDog() {
+    const name = document.getElementById('dog-name').value;
+    const dog = { name };
+    dogs.push(dog);
+    addDogToDisplay(dog);
 }
